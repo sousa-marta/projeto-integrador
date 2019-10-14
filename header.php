ce343903879f7e8bd767d9688b3819cf88b12cdf
Victor De Abreu <vmanuel.deabreu1@gmail.com>
2019-10-14 16:48:37 -0300

    Header pronto

---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header class="container">
+        <div class="row">
+            <div class="col-12 menu">
+                <div class="row">
+                    <div class="col-2 logo-header">
+                        <img class="logo-mato" src="img/a_logo-img_turquesa-dark.png" alt="logo-oppy">
+                        <img class="logo-logotipo" src="img/b_logo-nome_turquesa-dark-white.png" alt="logo-oppy">
+                    </div>
+                    <nav class="col-10 d-flex align-items-center justify-content-end">
+                        <ul class="nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Home</a>
+                            </li> 
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Quem somos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Cursos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Vagas emprego</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Apoio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Contribua</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Contato</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Cadastre-se</a>
+                            </li>
+                            <input class="btn btn-primary btn-dark" type="submit" value="Login">
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
