86741f0bde085f206d19c40458b0dafc90a60c81
lis <lismagol@gmail.com>
2019-10-17 13:47:56 -0300

    Finalizando página de Cadastro

--- register.php
+++ register.php
@@ -1,6 +1,8 @@
 <?php
 
-$countries = ["Andorra", "Emirados Árabes Unidos", "Afeganistão", "Antígua e Barbuda", "Anguilla", "Albânia", "Armênia", "Angola", "Antártica", "Argentina", "Samoa Americana"]
+$countries = ["Afeganistão","África do Sul","Akrotiri","Albânia","Alemanha","Andorra","Angola","Anguila","Antárctida","Antígua e Barbuda","Arábia Saudita","Arctic Ocean","Argélia","Argentina","Arménia","Aruba","Ashmore and Cartier Islands","Atlantic Ocean","Austrália","Áustria","Azerbaijão","Baamas","Bangladeche","Barbados","Barém","Bélgica","Belize","Benim","Bermudas","Bielorrússia","Birmânia","Bolívia","Bósnia e Herzegovina","Botsuana","Brasil","Brunei","Bulgária","Burquina Faso","Burúndi","Butão","Cabo Verde","Camarões","Camboja","Canadá","Catar","Cazaquistão","Chade","Chile","China","Chipre","Clipperton Island","Colômbia","Comores","Congo-Brazzaville","Congo-Kinshasa","Coral Sea Islands","Coreia do Norte","Coreia do Sul","Costa do Marfim","Costa Rica","Croácia","Cuba","Curacao","Dhekelia","Dinamarca","Domínica","Egipto","Emiratos Árabes Unidos","Equador","Eritreia","Eslováquia","Eslovénia","Espanha","Estados Unidos","Estónia","Etiópia","Faroé","Fiji","Filipinas","Finlândia","França","Gabão","Gâmbia","Gana","Gaza Strip","Geórgia","Geórgia do Sul e Sandwich do Sul","Gibraltar","Granada","Grécia","Gronelândia","Guame","Guatemala","Guernsey","Guiana","Guiné","Guiné Equatorial","Guiné-Bissau","Haiti","Honduras","Hong Kong","Hungria","Iémen","Ilha Bouvet","Ilha do Natal","Ilha Norfolk","Ilhas Caimão","Ilhas Cook","Ilhas dos Cocos","Ilhas Falkland","Ilhas Heard e McDonald","Ilhas Marshall","Ilhas Salomão","Ilhas Turcas e Caicos","Ilhas Virgens Americanas","Ilhas Virgens Britânicas","Índia","Indian Ocean","Indonésia","Irão","Iraque","Irlanda","Islândia","Israel","Itália","Jamaica","Jan Mayen","Japão","Jersey","Jibuti","Jordânia","Kosovo","Kuwait","Laos","Lesoto","Letónia","Líbano","Libéria","Líbia","Listenstaine","Lituânia","Luxemburgo","Macau","Macedónia","Madagáscar","Malásia","Malávi","Maldivas","Mali","Malta","Man, Isle of","Marianas do Norte","Marrocos","Maurícia","Mauritânia","México","Micronésia","Moçambique","Moldávia","Mónaco","Mongólia","Monserrate","Montenegro","Mundo","Namíbia","Nauru","Navassa Island","Nepal","Nicarágua","Níger","Nigéria","Niue","Noruega","Nova Caledónia","Nova Zelândia","Omã","Pacific Ocean","Países Baixos","Palau","Panamá","Papua-Nova Guiné","Paquistão","Paracel Islands","Paraguai","Peru","Pitcairn","Polinésia Francesa","Polónia","Porto Rico","Portugal","Quénia","Quirguizistão","Quiribáti","Reino Unido","República Centro-Africana","República Checa","República Dominicana","Roménia","Ruanda","Rússia","Salvador","Samoa","Samoa Americana","Santa Helena","Santa Lúcia","São Bartolomeu","São Cristóvão e Neves","São Marinho","São Martinho","São Pedro e Miquelon","São Tomé e Príncipe","São Vicente e Granadinas","Sara Ocidental","Seicheles","Senegal","Serra Leoa","Sérvia","Singapura","Sint Maarten","Síria","Somália","Southern Ocean","Spratly Islands","Sri Lanca","Suazilândia","Sudão","Sudão do Sul","Suécia","Suíça","Suriname","Svalbard e Jan Mayen","Tailândia","Taiwan","Tajiquistão","Tanzânia","Território Britânico do Oceano Índico","Territórios Austrais Franceses","Timor Leste","Togo","Tokelau","Tonga","Trindade e Tobago","Tunísia","Turquemenistão","Turquia","Tuvalu","Ucrânia","Uganda","União Europeia","Uruguai","Usbequistão","Vanuatu","Vaticano","Venezuela","Vietname","Wake Island","Wallis e Futuna","West Bank","Zâmbia","Zimbabué"]
+
+
 
 ?>
 
@@ -20,12 +22,12 @@ $countries = ["Andorra", "Emirados Árabes Unidos", "Afeganistão", "Antígua e 
   
   <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
-      <li class="breadcrumb-item"><a class="breadcrumb-item-link" href="#">Home</a></li>
+      <li class="breadcrumb-item"><a class="breadcrumb-item-link" href="index.php">Home</a></li>
       <li class="breadcrumb-item active" aria-current="page">Cadastre-se</li>
     </ol>
   </nav>
   
-  <main class="d-flex justify-content-center align-items-center p-2">
+  <main class="d-flex justify-content-center align-items-center p-4 mb-5">
     <form action="" method="post" class="card p-5 col-lg-5">
       <div class="form-group text-center">
         <h3>Estamos felizes por você querer fazer parte da nossa comunidade</h3>
@@ -46,7 +48,7 @@ $countries = ["Andorra", "Emirados Árabes Unidos", "Afeganistão", "Antígua e 
       <label for="userCountry">País de origem</label>
       <div class="form-group">
         <select class="col-lg-12" name="userCountry" id="userCountry">
-          <option value="">Escolha seu país de origem</option>
+          <option value="">Selecione seu país de origem</option>
           <?php
           for ($i = 1; $i < count($countries);$i++) {
             foreach ($countries as $country) {
@@ -56,8 +58,8 @@ $countries = ["Andorra", "Emirados Árabes Unidos", "Afeganistão", "Antígua e 
           ?>
         </select>
       </div>
-      <button type="submit" class="btn btn-redefine-color align-self-center">Criar conta gratuitamente</button>
-      <p class="text-center mt-2">Ao criar minha conta, eu aceito os Termos de Uso e a Política de Privacidade da Oppy.</p>
+      <button type="submit" class="btn btn-redefine-color align-self-center mt-3">Criar conta gratuitamente</button>
+      <p class="text-center mt-3">Ao criar minha conta, eu aceito os Termos de Uso e a Política de Privacidade da Oppy.</p>
     </form>
   </main>
 </body>
