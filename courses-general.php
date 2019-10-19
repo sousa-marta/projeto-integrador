730a33b70513e57b60c726f5c438f3ca89471cbe
Marta Sousa <marta.sousa@gmail.com>
2019-10-19 18:11:35 -0300

    página cursos geral iniciada

---
+++ courses-general.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Cursos</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Baloo&display=swap" rel="stylesheet">
+</head>
+<body>
+  <!-- Header -->
+  <?php include_once("header.php"); ?>
+  
+  <!-- Navegação Breadcrumb  -->
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a class="breadcrumb-item-link" href="#">Home</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Cursos</li>
+    </ol>
+  </nav>
+
+  <main class="container-fluid">
+    <!-- Banner -->
+    <div class="row">
+      <section class="courses-g-banner col-md-12 d-flex justify-content-center align-items-center">
+        <h1>Cursos</h1>
+      </section>
+    </div>
+    
+    <!-- Conteúdo Central da Página -->
+    <div class="row">
+      <section class=" col-md-9">
+      </section>
+    </div>
+
+    <!-- Barra de Navegação Lateral -->
+    <div class="row">
+      <aside class="courses-g-aside col-md-3">
+        <h2>Categorias</h2>
+        <h4>Idiomas</h3>
+        <ul>
+          <li>Portugês</li>
+          <li>Espanhol</li>
+          <li>Inglês</li>
+        </ul>
+      </aside>
+    </div>
+
+  </main>
+
+
+
+
+  
+
+
+
+  <!-- Footer -->
+  <?php include_once("footer.php") ?>
+
+</body>
+</html>
