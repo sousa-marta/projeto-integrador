cf72b50b835d894bd0afe047e1716693e76c144b
Alex Watanabe <aowatanabe@gmail.com>
2019-10-20 11:08:44 -0300

    adicionando parte de informações principais

--- user-page.php
+++ user-page.php
@@ -10,8 +10,53 @@
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+  <!-- CABEÇALHO -->
   <?php include_once('header.php'); ?>
-  
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a class="breadcrumb-item-link" href="#">Home</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Página do Usuário</li>
+    </ol>
+  </nav>
+
+  <main>
+
+    <!-- INFORMAÇÕES -->
+    <section class="user-page-background-dark-green">
+      <div class="container d-flex justify-content-around p-3 align-items-center">
+        <img src="img/foto.jpeg" alt="foto usuário" class="figure-img img-fluid rounded user-page-photo">
+        <div>
+          <h1 class="user-page-font-white user-page-font-size-30">Alex Watanabe</h1>
+          <p class="user-page-font-white"><b>País receptor:</b> Brasil</p>
+        </div>
+      </div>
+    </section>
+
+    <!-- VAGAS -->
+    <section class="container">
+      <div class="row">
+
+        <!-- vagas em andamento -->
+        <div class="col-4 p-3">
+          <h2 class="user-page-background-dark-green user-page-font-white">Vagas em Andamento</h2>
+            <h2>
+            
+          </div>
+        </div>
+
+
+
+
+
+      </div>
+    </section>
+
+    <!-- CURSOS -->
+
+  </main>
+
+
+  <!-- FOOTER -->
   
 </body>
 </html>
