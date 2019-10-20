d20c4af6abf013288142f9cd7d946344e1184c14
Marta Sousa <marta.sousa@gmail.com>
2019-10-20 11:13:08 -0200

    arrumei gutters e espaçamentos

--- courses-general.php
+++ courses-general.php
@@ -22,7 +22,7 @@
     </ol>
   </nav>
 
-  <main class="container-fluid">
+  <main>
     <div class="row no-gutters">
       <!-- Banner -->
       <section class="courses-g-banner col-md-12 d-flex justify-content-center align-items-center">
@@ -30,7 +30,7 @@
       </section>
  
       <!-- Conteúdo Central da Página -->
-      <div class="col-md-9 order-2">
+      <div class="col-lg-9 col-md-8 order-2">
 
         <!-- Campo Pesquisa por Curso -->
         <section class="courses-g-search border-bottom">
@@ -46,31 +46,31 @@
         
         <!-- Categorias de Cursos Imagens -->
         <section class="row justify-content-center my-4">
-          <div class="col-md-5">
+          <div class="col-lg-5 col-md-7 d-flex flex-column align-items-center">
             <div class="courses-g-cards my-4">
               <h4 class="card-title text-center py-2">Idiomas</h4>
               <img src="img/courses-g-languages.jpg" alt="página de livro aberta com poema" class="card-img-bottom">
             </div>
           </div>
-          <div class="col-md-5">
+          <div class="col-lg-5 col-md-7 d-flex flex-column align-items-center">
             <div class="courses-g-cards my-4">
               <h4 class="card-title text-center py-2">Saúde</h4>
               <img src="img/courses-g-health.jpg" alt="página de livro aberta com poema" class="card-img-bottom">
             </div>
           </div>
-          <div class="col-md-5">
+          <div class="col-lg-5 col-md-7 d-flex flex-column align-items-center">
             <div class="courses-g-cards my-4">
               <h4 class="card-title text-center py-2">Beleza</h4>
               <img src="img/courses-g-beauty.jpg" alt="página de livro aberta com poema" class="card-img-bottom">
             </div>
           </div>
-          <div class="col-md-5">
+          <div class="col-lg-5 col-md-7 d-flex flex-column align-items-center">
             <div class="courses-g-cards my-4">
               <h4 class="card-title text-center py-2">Programação</h4>
               <img src="img/courses-g-programming.jpg" alt="página de livro aberta com poema" class="card-img-bottom">
             </div>
           </div>
-          <div class="col-md-5">
+          <div class="col-lg-5 col-md-7 d-flex flex-column align-items-center">
             <div class="courses-g-cards my-4">
               <h4 class="card-title text-center py-2">Direção</h4>
               <img src="img/courses-g-car.jpg" alt="página de livro aberta com poema" class="card-img-bottom">
@@ -82,8 +82,8 @@
       </div>
 
       <!-- Barra de Navegação Lateral -->
-      <div class="col-md-3 order-1 ">
-        <aside class=" courses-g-aside ">
+      <aside class="col-lg-3 col-md-4 order-1 courses-g-aside">
+        <div class="d-flex flex-column p-5">
           <h2>Categorias</h2>
           <h4>Idiomas</h3>
           <ul>
@@ -114,8 +114,9 @@
             <li>Conhecimentos Teóricos</li>
             <li>Reciclagem de Motorista</li>
           </ul>
-        </aside>
-      </div>
+        </div>
+      </aside>
+      
     </div>
 
   </main>
