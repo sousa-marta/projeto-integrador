85a6a61a1e77edf39b2a09ae0343768d2dfdaccc
lis <lismagol@gmail.com>
2019-10-21 18:55:17 -0300

    Inicializando a página Quem Somos

---
+++ about.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Quem Somos</title>
+  <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <?php include_once('header.php'); ?>
+  
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb mb-0">
+      <li class="breadcrumb-item"><a class="breadcrumb-item-link" href="#">Home</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Quem somos</li>
+    </ol>
+  </nav>
+  
+  <main>
+    <!-- Banner -->
+    <section class="about-banner border-bottom">
+    <blockquote class="blockquote text-center">
+      <p class="mb-0">Para a maioria dos imigrantes, mudar para um país novo é um ato de fé. Mesmo que você tenha ouvido histórias sobre segurança, oportunidade e prosperidade,  ainda assim é um grande salto se afastar de sua língua, de seu povo e de seu país. De suas raízes.</p>
+      <footer class="blockquote-footer">Nicola Yoon,  escritora jamaicana-americana<cite title="Source Title">Source Title</cite></footer>
+    </section>
+
+    <!-- Texto Resumo Sobre o Site -->
+    <section class="about-summary">
+      <div class="container">
+        <p>A Oppy foi criada em 2019 com o objetivo de dar a oportunidade dos emigrantes saídos de diversas condições de vida obterem as habilidades técnicas e emocionais para entrar no mercado de trabalho de um novo país e melhorar suas condições de vida. 
+          
+        <p> Contamos com apoio de empresas e recrutadores para conectar essa mão-de-obra qualificada e cheia de energia para aulixiar a integrassão dos novos habitantes do país! </p>
+        
+        <a href="#" class="btn home-btn-trq">Saiba Mais</a>
+      </div>
+    </section>
+  </main>
+  <footer>
+    <?php include_once('footer.php'); ?>
+  </footer>
+</body>
+</html>
