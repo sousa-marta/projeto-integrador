5d0b885e4853d44f34594bd27f122d20e38533cf
@ricardoveiga27 <ricardo.veiga27@gmail.com>
2019-10-23 10:26:27 -0300

    pagina oportunidades, assessoria juridica, mentoria profissional

---
+++ legal-assistance.php
@@ -0,0 +1,47 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Assistencia Jurídica</title>
+  <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <?php include_once('header.php'); ?>
+
+  <main class="container p-3 mb-5">
+
+    <section class="container p-3">
+      <div class="row align-items-center">
+        <div class="col-lg-4 col-md-4 banner-img">
+          <img class="img-fluid" src="img/juridico.png" alt="apoio juridico">
+        </div>
+        <div class="col-lg-8 col-md-8 banner-img">
+          <h1 class="h1-apoio">Conheça a Assistência Jurídica da Oppy</h1>
+        </div>
+      </div>
+    </section>
+    <h3 class="container  text-justify font-italic p-3"> A assessoria jurídica a refugiados e a imigrantes é fundamental devido ao alto grau de desconhecimento da legislação brasileira por parte dessas pessoas e da sociedade em geral.
+      <br><br> Um migrante que passa a habitar o país sente necessidade de orientação em assuntos legais para poder se integrar devidamente ao ambiente e para ter consciência dos seus direitos e deveres.
+      <br><br> As questões tratadas pela OPPY já envolvem pedidos de naturalização e de nacionalização, vistos, previdência, casamento, acesso à educação e à saúde, entre outras.
+      <br><br> A OPPY possui um forte trabalho em rede, destacando-se a pareceria com Alto Comissariado das Nações Unidas para Refugiados (ACNUR), com a Associação Antonio Vieira (ASAV) e com o CIBAI Migrações. Ademais, integra o Fórum Permanente de Mobilidade Humana (FPMH) o Comitê Estadual de Atenção a Migrantes, Refugiados, Apátridas e Vítimas de Tráfico de Pessoas (COMIRAT-SP) e tem representantes junto ao Comitê de Acompanhamento pela Sociedade Civil sobre ações de Migração e Refúgio (CASC-Migrante) da Secretaria Nacional de Justiça.
+    </h3>
+    <div class="d-flex justify-content-center">
+      <button type="button" class="btn btn-primary btn-lg">Clique aqui para entrar em contato!</button>
+
+    </div>
+
+  </main>
+  <?php include_once('footer.php') ?>
+</body>
+
+</html>
