58f2bcfc4d5a0889899f6f39b942fcfcd4f36652
Victor De Abreu <vmanuel.deabreu1@gmail.com>
2019-11-05 11:06:38 -0300

    Arrumacoes na página do Apoio. Troca do nome no menú para evitar confusao com a página Oportunidades

--- header.php
+++ header.php
@@ -24,16 +24,8 @@
             <li class="nav-item">
               <a class="nav-link active" href="opportunities.php">Oportunidades</a>
             </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Apoio
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="support.php">Geral</a>
-                <a class="dropdown-item" href="support-legal.php">Jurídico</a>
-                <a class="dropdown-item" href="support-professional.php">Profissional</a>
-                <a class="dropdown-item" href="support-psychologic.php">Psicológico</a>
-              </div>
+            <li class="nav-item">
+              <a class="nav-link active" href="support.php">Apoio</a>
             </li>
             <li class="nav-item">
               <a class="nav-link active" href="contribute.php">Contribua</a>
