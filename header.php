a797321ad7e8c6971b1dad9f75c428f1464377d7
lis <lismagol@gmail.com>
2019-11-12 19:23:10 -0300

    Corrigindo problema do header e revisão geral do CSS

--- header.php
+++ header.php
@@ -1,14 +1,13 @@
 <header>
   <nav class="navbar navbar-expand-lg navbar-light menu fixed-top">
-    <div class="col-1 pr-0 pl-0 logo-header row align-items-center">
+    <div class="col-1 col-lg-2 pr-0 pl-0 logo-header row align-items-center">
+      <a href="index.php" role="logo"><img class="logo-img" src="img/a_logo-img_turquesa-dark.png" alt="Logo da Oppy: desenho de árvore com pessoa de pé embaixo dos galhos"></a>
       <a href="index.php" role="logo"><img class="logo-name" src="img/b_logo-nome_turquesa-dark-white.png" alt="Oppy"></a>
-      <a href="index.php" role="logo"><img class="logo-column" src="img/a_logo_turquesa-dark.png" alt="Logo da Oppy: desenho de árvore com pessoa de pé embaixo dos galhos"></a>
-      <a href="index.php" role="logo"><img class="logo-row" src="img/a_logo-img_turquesa-dark-row.png" alt="Logo da Oppy: desenho de árvore com pessoa de pé embaixo dos galhos"></a>
     </div>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Menu recolhido">
     <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="col-11 collapse navbar-collapse justify-content-end" id="navbarNav">
+    <div class="col-md-10 pl-0 pr-0 collapse navbar-collapse justify-content-end align-items-center" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item active">
             <a class="nav-link" href="index.php"><span class="fa">&#xf015;</span> Home <span class="sr-only">(current)</span></a>
