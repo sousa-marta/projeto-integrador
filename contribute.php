143f05d2bdb82a3aaab86db1900c719ccf6b8c4a
lis <lismagol@gmail.com>
2019-11-11 21:03:45 -0300

    Revisão da página Cursos

--- contribute.php
+++ contribute.php
@@ -15,6 +15,7 @@
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+  <!-- Header -->
   <?php include_once('header.php'); ?>
 
   <!-- Breadcrumb  -->
@@ -124,6 +125,7 @@
     </section>
   </main>
 
+  <!-- Footer -->
   <?php include_once('footer.php'); ?>
 
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
