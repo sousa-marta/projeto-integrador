77beeb08098974e17613d4d679cae3587d9aed3d
lis <lismagol@gmail.com>
2019-11-21 08:53:37 -0300

    Ajustando margens da página Contato

--- contact.php
+++ contact.php
@@ -35,8 +35,8 @@ $subjects = ["Apoio","Cadastro","Cursos","Doação","Voluntariado","Outros"];
   <main class="container-fluid">
     <!-- Formulário de Contato -->
     <h1 class="pages-title-bg d-flex justify-content-center align-items-center">Contato</h1>
-    <div class="row align-self-center justify-content-center mb-3">
-    <form action="" method="post" class="card col-sm-8 col-lg-6 contact-form-design">
+    <div class="row align-self-center justify-content-center m-4">
+    <form action="" method="post" class="card col-11 col-sm-8 col-lg-6 contact-form-design">
       <p class="text-center">Caso não encontre a resposta para a sua pergunta abaixo, não hesite em nos contatar usando o formulário ou diretamente pelo e-mail: <a class="contact-email" href="mailto:">contato@oppy.com.br</a></p>
       <div class="col-lg-8 align-self-center mt-2">
       <div class="form-group">
@@ -49,7 +49,7 @@ $subjects = ["Apoio","Cadastro","Cursos","Doação","Voluntariado","Outros"];
       </div>
       <label for="contactSubject"><h5>Assunto</h5></label>
       <div class="form-group">
-        <select class="col-lg-12" name="contactSubject" id="contactSubject">
+        <select class="col-lg-12 form-control" name="contactSubject" id="contactSubject">
           <option value="">Selecione o assunto</option>
           <?php
           foreach ($subjects as $subject) {
@@ -71,11 +71,11 @@ $subjects = ["Apoio","Cadastro","Cursos","Doação","Voluntariado","Outros"];
     <!-- Perguntas e Respostas -->
     <h1 class="pages-title-bg d-flex justify-content-center align-items-center m-0">FAQ</h1>
     
-    <div class="accordion pl-3 pr-3 pb-5 questions-bg" id="questionsTopicOne">
-      <h2 class="pt-5 questions-subject">Tópico 1</h2>
+    <div class="accordion px-5 pb-5 questions-bg" id="questionsTopicOne">
+      <h3 class="py-4 questions-subject">Tópico 1</h3>
     <div class="card">
       <div class="card-header border-0 d-flex justify-content-between align-items-center" id="questionOne">
-        <h4 class="m-0 questions-color">Pergunta 1</h4>
+        <h5 class="m-0 questions-color">Pergunta 1</h5>
         <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
           <img src="img/plus-sign-to-add.svg" class="plus-sign" alt="">
         </button>
@@ -88,7 +88,7 @@ $subjects = ["Apoio","Cadastro","Cursos","Doação","Voluntariado","Outros"];
     </div>
     <div class="card">
       <div class="card-header border-0 d-flex justify-content-between align-items-center" id="questionTwo">
-        <h4 class="mb-0 questions-color">Pergunta 2</h4>
+        <h5 class="mb-0 questions-color">Pergunta 2</h5>
         <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
           <img src="img/plus-sign-to-add.svg" class="plus-sign" alt="">
         </button>
@@ -101,7 +101,7 @@ $subjects = ["Apoio","Cadastro","Cursos","Doação","Voluntariado","Outros"];
     </div>
     <div class="card">
       <div class="card-header border-0 d-flex justify-content-between align-items-center" id="questionThree">
-        <h4 class="mb-0 questions-color">Pergunta 3</h4>
+        <h5 class="mb-0 questions-color">Pergunta 3</h5>
         <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
           <img src="img/plus-sign-to-add.svg" class="plus-sign" alt="">
         </button>
