1091487b14acb35635cf6f04142fee70a509643b
Lis <53536548+lislaineo@users.noreply.github.com>
2019-12-07 08:46:00 -0300

    Remoção de breadcrumb da página Apoio

--- resources/views/support.blade.php
+++ resources/views/support.blade.php
@@ -5,14 +5,6 @@ Apoio
 @endsection
 
 @section('content')
-<!-- Breadcrumb  -->
-<nav aria-label="breadcrumb">
-    <ol class="breadcrumb">
-    <li class="breadcrumb-item"><a class="breadcrumb-item-link" href="index.php">Home</a></li>
-    <li class="breadcrumb-item active" aria-current="page">Apoio</li>
-    </ol>
-</nav>
-
 <!-- Banner -->
 <section class="container p-3">      
     <div class="row banner-support-page text-center">
