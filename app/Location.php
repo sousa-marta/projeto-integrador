7e6231b3d4719357b5ece6090758926acdaeda77
Alex Watanabe <aowatanabe@gmail.com>
2019-12-18 17:23:35 -0300

    montado seeder de locations que será apenas país, deletei AdminController porque vai mudar para ser o controle de uma base de dados de administradores

--- app/Location.php
+++ app/Location.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
+
+  protected $fillable = ['country'];
+
   public function vacancies()
   {
     return $this->hasMany('App\Vacancy');
