ab4ed01fe71c599db4248005264d18adf06bd9a6
Lis <lismagol@gmail.com>
2020-02-10 14:06:27 -0300

    Finalizando página de erro 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,7 +4,9 @@
 
 @section('content')
 <div class="pt-5 mt-3">
-  <div class="container col-5 card my-5">
+  <div class="container col-8 card my-5">
+  <div class="row align-items-center">
+    <img src="img/undraw_warning_cyit.svg" alt="" class="col-5 my-3">
     <div class="card-body text-center">
       <h5>Não encontramos o que você procurou.</h5>
       <p>O que pode ter acontecido?</p>
@@ -12,8 +14,9 @@
         <li class="pt-2">O conteúdo não está mais no ar.</li>
         <li class="pt-2">O endereço digitado está errado.</li>
       </ul>
-      <button type="button" onclick="location.href='/'" class="btn btn-secondary"><i class='fas fa-home mr-2'></i> Ir para página inicial</button>
+      <button type="button" onclick="location.href='/'" class="btn btn-secondary"><i class='fas fa-home mr-2'></i>Ir para página inicial</button>
     </div>
+  </div>
   </div>
 </div>
 @endsection
